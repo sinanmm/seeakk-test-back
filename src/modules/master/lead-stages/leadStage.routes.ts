6234c32c9e1a4d029eca239225ff8408afc7b285
Harshad M T <harshadmuhammad168@gmail.com>
2026-04-28 11:43:38 +0530

    fix(leads): broaden metadata access for lead forms

--- src/modules/master/lead-stages/leadStage.routes.ts
+++ src/modules/master/lead-stages/leadStage.routes.ts
@@ -1,12 +1,38 @@
 import { Router } from 'express';
-import { checkPermission, protect } from '../../../middlewares/authMiddleware';
+import { checkAnyPermission, checkPermission, protect } from '../../../middlewares/authMiddleware';
 import * as leadStageController from './leadStage.controller';
 
 const router = Router();
 
 router.post('/', protect, checkPermission('LEAD_STAGES_CREATE'), leadStageController.createLeadStage);
-router.get('/', protect, checkPermission('LEAD_STAGES_VIEW'), leadStageController.listLeadStages);
-router.get('/pipeline', protect, checkPermission('LEAD_STAGES_VIEW'), leadStageController.getPipelineLeadStages);
+router.get(
+  '/',
+  protect,
+  checkAnyPermission([
+    'LEAD_STAGES_VIEW',
+    'LEADS_CREATE',
+    'LEADS_EDIT',
+    'LEADS_VIEW_ALL',
+    'LEADS_VIEW_TEAM',
+    'LEADS_VIEW_OWN',
+    'SYSTEM_CONFIG',
+  ]),
+  leadStageController.listLeadStages,
+);
+router.get(
+  '/pipeline',
+  protect,
+  checkAnyPermission([
+    'LEAD_STAGES_VIEW',
+    'LEADS_CREATE',
+    'LEADS_EDIT',
+    'LEADS_VIEW_ALL',
+    'LEADS_VIEW_TEAM',
+    'LEADS_VIEW_OWN',
+    'SYSTEM_CONFIG',
+  ]),
+  leadStageController.getPipelineLeadStages,
+);
 router.patch('/reorder', protect, checkPermission('LEAD_STAGES_EDIT'), leadStageController.reorderLeadStages);
 router.put('/:id', protect, checkPermission('LEAD_STAGES_EDIT'), leadStageController.updateLeadStage);
 router.patch('/:id/status', protect, checkPermission('LEAD_STAGES_EDIT'), leadStageController.toggleLeadStageStatus);
