57d8bc967501eddb054f6fdd2897c0effdf25ae8
Harshad M T <harshadmuhammad168@gmail.com>
2026-05-19 10:24:56 +0530

    security: allow read-only access to countries/locations for all authenticated users

--- src/modules/master/locations/locations.routes.ts
+++ src/modules/master/locations/locations.routes.ts
@@ -7,18 +7,17 @@ const router = Router();
 router.get(
   '/countries',
   protect,
-  checkAnyPermission(['LOCATION_VIEW', 'LOCATION_MANAGE']),
   locationsController.listCountries,
 );
 router.post('/countries', protect, checkPermission('LOCATION_MANAGE'), locationsController.createCountry);
 router.put('/countries/:id', protect, checkPermission('LOCATION_MANAGE'), locationsController.updateCountry);
 router.delete('/countries/:id', protect, checkPermission('LOCATION_MANAGE'), locationsController.deleteCountry);
 
-router.get('/levels', protect, checkAnyPermission(['LOCATION_VIEW', 'LOCATION_MANAGE']), locationsController.listLocationLevels);
+router.get('/levels', protect, locationsController.listLocationLevels);
 router.post('/levels', protect, checkPermission('LOCATION_MANAGE'), locationsController.configureLocationLevels);
 
-router.get('/tree', protect, checkAnyPermission(['LOCATION_VIEW', 'LOCATION_MANAGE']), locationsController.getLocationTree);
-router.get('/', protect, checkAnyPermission(['LOCATION_VIEW', 'LOCATION_MANAGE']), locationsController.listLocations);
+router.get('/tree', protect, locationsController.getLocationTree);
+router.get('/', protect, locationsController.listLocations);
 router.post('/', protect, checkPermission('LOCATION_MANAGE'), locationsController.createLocation);
 router.put('/:id', protect, checkPermission('LOCATION_MANAGE'), locationsController.updateLocation);
 router.delete('/:id', protect, checkPermission('LOCATION_MANAGE'), locationsController.deleteLocation);
